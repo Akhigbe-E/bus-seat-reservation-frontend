da0ecf567990467b3007649426d7c31c7825d0cb
Akhigbe-E <akhigbemmanuel@gmail.com>
2020-01-04 19:50:17 +0100

    Converted PickTimeAndLocation to scss

--- src/Components/Reservation/PickTimeAndLocation/PickTimeAndLocation.js
+++ src/Components/Reservation/PickTimeAndLocation/PickTimeAndLocation.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./PickTimeAndLocation.css";
+import "./PickTimeAndLocation.scss";
 
 export const PickTimeAndLocation = props => {
   const buses = [
