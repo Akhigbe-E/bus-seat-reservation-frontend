c5831f6000a91d590130a2a6fc96269c7d3d5689
Akhigbe-E <akhigbemmanuel@gmail.com>
2020-01-06 13:26:52 +0100

    Styling the Ticket component

--- src/Components/Ticketing/Ticket.js
+++ src/Components/Ticketing/Ticket.js
@@ -5,7 +5,12 @@ export const Ticket = props => {
   return (
     <div className="ticket">
       <div className="ticket-main animated slideInUp delay-1s">
-        <p className="question-head">Booking Pass</p>
+        <p
+          className="question-head"
+          style={{ textAlign: "left", paddingBottom: "10px" }}
+        >
+          Booking Pass
+        </p>
         <div className="direction-location">
           <p className="location">CST</p>
           <img
@@ -15,8 +20,26 @@ export const Ticket = props => {
           />
           <p className="location">CST</p>
         </div>
-        <div className="vehicle-seat"></div>
-        <div className="time"></div>
+        <div className="vehicle-seat">
+          <div className="ticket-section">
+            <p className="ticket-section-head">VEHICLE</p>
+            <p className="ticket-section-body">Hilus Bus</p>
+          </div>
+          <div className="ticket-section two">
+            <p className="ticket-section-head">SEAT</p>
+            <p className="ticket-section-body">5</p>
+          </div>
+        </div>
+        <div className="time">
+          <div className="ticket-section">
+            <p className="ticket-section-head">BOARDING</p>
+            <p className="ticket-section-body">2:30 PM</p>
+          </div>
+          <div className="ticket-section two">
+            <p className="ticket-section-head">TIME LEFT</p>
+            <p className="ticket-section-body">1:50:30</p>
+          </div>
+        </div>
         <div className="passenger-barcode"></div>
       </div>
     </div>
