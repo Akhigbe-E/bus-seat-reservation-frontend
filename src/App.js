9c8deffaef88f9fe0d87416b7aedf50db2d0672e
Emmanuel Akhigbe <akhigbemmanuel@gmail.com>
2020-01-26 12:38:19 -0800

    minor updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Link, BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import { Authentication } from "./Components/Authentication/Authentication";
 import { PickTimeAndLocation } from "./Components/Reservation/PickTimeAndLocation/PickTimeAndLocation";
-import { Reservation } from "./Components/Reservation/Reservation";
+// import { Reservation } from "./Components/Reservation/Reservation";
 import { PickDestination } from "./Components/Reservation/PickDestination/PickDestination";
 import { SelectSeat } from "./Components/Reservation/SelectSeat/SelectSeat";
 import { Ticket } from "./Components/Ticketing/Ticket";
