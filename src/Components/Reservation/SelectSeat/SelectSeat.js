8e4c1e44adaf1fd7e51e1ee55badcd6568c315ee
Emmanuel Akhigbe <akhigbemmanuel@gmail.com>
2020-01-27 00:49:21 -0800

    Developing the Seat Component

--- src/Components/Reservation/SelectSeat/SelectSeat.js
+++ src/Components/Reservation/SelectSeat/SelectSeat.js
@@ -2,23 +2,8 @@ import React, { useState } from "react";
 import "./SelectSeat.scss";
 
 export const SelectSeat = props => {
-  const [selected, setSelected] = useState(false);
-  const [selected2, setSelected2] = useState(false);
-  const [selected3, setSelected3] = useState(false);
-
   const [seats, setSeats] = useState([
-    // { seatOne: "available" },
-    // { seatTwo: "available" },
-    // { seatThree: "available" },
-    // { seatFour: "available" },
-    // { seaFive: "available" },
-    // { seatSix: "available" },
-    // { seatSeven: "available" },
-    // { seatEight: "available" },
-    // { seatNine: "available" },
-    // { seatTen: "available" },
-    // { seatEleven: "available" },
-    // { seatEleven: "available" }
+    { seatNo: 0, available: true },
     { seatNo: 1, available: true },
     { seatNo: 2, available: true },
     { seatNo: 3, available: true },
@@ -28,19 +13,10 @@ export const SelectSeat = props => {
     { seatNo: 7, available: true },
     { seatNo: 8, available: true },
     { seatNo: 9, available: true },
-    { seatNo: 10, available: true },
-    { seatNo: 11, available: true }
+    { seatNo: 10, available: true }
   ]);
-  const selectSeat = () => {
-    setSelected(!selected);
-  };
-  const selectSeat2 = () => {
-    setSelected2(!selected);
-  };
-  const selectSeat3 = () => {
-    setSelected3(!selected);
-  };
-  const firstRow = () => {
+
+  const firstRow = seats => {
     const firstRow = [];
     for (let i = 0; i < 3; i++) {
       firstRow.push(seats[i]);
@@ -51,16 +27,28 @@ export const SelectSeat = props => {
   const secondRow = () => {
     const secondRow = [];
     for (let i = 3; i < 7; i++) {
-      firstRow.push(seats[i]);
+      secondRow.push(seats[i]);
     }
     return secondRow;
   };
   const thirdRow = () => {
     const thirdRow = [];
-    for (let i = 7; i < 12; i++) {
+    for (let i = 7; i < 11; i++) {
       thirdRow.push(seats[i]);
     }
     return thirdRow;
+  };
+
+  const handleSeatClick = seatNumber => {
+    let seatsCopy = seats;
+    for (let i = 0; i < seats.length; i++) {
+      seatsCopy[i].available = true;
+      setSeats([...seatsCopy]);
+      if (seats[i].seatNo === seatNumber) {
+        seatsCopy[i].available = false;
+        setSeats([...seatsCopy]);
+      }
+    }
   };
   return (
     <div className="select-seat">
@@ -69,47 +57,84 @@ export const SelectSeat = props => {
         <p>Click on the seat to book</p>
       </div>
       <div className="bus">
-        {firstRow().map(({ seatNo, available }) => (
-          <div>
-            <button
-              value="seat1"
-              className={available ? "seat-dormant" : "selected-seat"}
-              onClick={() => {
-                setSeats([...seats, { seatNo: seatNo, available: false }]);
-              }}
-            ></button>
-          </div>
-        ))}
-
-        <div className="bus-seat">
-          <button
-            value="seat1"
-            className={selected ? "selected-seat" : "seat-dormant"}
-            onClick={() => {
-              selectSeat();
-            }}
-          ></button>
-          <div className="bus-seat">
-            <button
-              value="seat1"
-              className={selected2 ? "selected-seat" : "seat-dormant"}
-              onClick={() => {
-                selectSeat2();
-              }}
-            ></button>
-          </div>
-          <div className="bus-seat">
-            <button
-              value="seat1"
-              className={selected3 ? "selected-seat" : "seat-dormant"}
-              onClick={() => {
-                selectSeat3();
-              }}
-            ></button>
-          </div>
-          {/* <button value="seat1">SEAT</button>
-          <button value="seat1">SEAT</button> */}
+        <div className="first-row">
+          {firstRow(seats).map(({ seatNo, available }) => {
+            let i = 0;
+            let customClasses = "seat";
+            return (
+              <div className="seat">
+                <button
+                  value={`seat${i++}`}
+                  className={
+                    available
+                      ? `seat-dormant ${customClasses}`
+                      : `selected-seat ${customClasses}`
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    handleSeatClick(seatNo);
+                  }}
+                ></button>
+              </div>
+            );
+          })}
         </div>
+        <div className="second-row">
+          {secondRow(seats).map(({ seatNo, available }) => {
+            let i = 0;
+            let customClasses = "seat";
+            return (
+              <div className="seat">
+                <button
+                  value={`seat${i++}`}
+                  className={
+                    available
+                      ? `seat-dormant ${customClasses}`
+                      : `selected-seat ${customClasses}`
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    handleSeatClick(seatNo);
+                  }}
+                ></button>
+              </div>
+            );
+          })}
+        </div>
+        <div className="third-row">
+          {thirdRow(seats).map(({ seatNo, available }) => {
+            let i = 0;
+            let customClasses = "seat";
+            return (
+              <div className="seat">
+                <button
+                  value={`seat${i++}`}
+                  className={
+                    available
+                      ? `seat-dormant ${customClasses}`
+                      : `selected-seat ${customClasses}`
+                  }
+                  onClick={e => {
+                    e.preventDefault();
+                    handleSeatClick(seatNo);
+                  }}
+                ></button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="tags">
+        <p style={{ backgroundColor: "#454545" }}>Reserved</p>
+        <p style={{ backgroundColor: "#46a530" }}>Empty</p>
+        <p style={{ backgroundColor: "rgb(231, 47, 47)" }}>Taken</p>
+      </div>
+      <div className="proceed-button-container">
+        {seats.some(({ available }) => available === false) ? (
+          <button className="proceed-button">Get Ticket</button>
+        ) : (
+          undefined
+        )}
       </div>
     </div>
   );
